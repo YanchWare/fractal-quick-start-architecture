13657b717de3626d5e9aab7dbba217629a9fcd9f
Angelo <aan@yanchware.com>
2025-02-04 11:02:02 +0100

    Fix class name

--- deploy/app/build.gradle.kts
+++ deploy/app/build.gradle.kts
@@ -34,7 +34,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.App"
+    mainClass = "com.yanchware.fractalcloud.samples.quickstart.architecture.deploy.App"
 }
 
 tasks.named<Test>("test") {
